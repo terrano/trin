707b254038c827f9dbf346bfe5a35cba036bc291
Ihor Kohut <ihor.o.kohut@gmail.com>
2024-11-11 15:30:02 +0200

    KMS config

---
+++ kms/kms.tf
@@ -0,0 +1,19 @@
+###################################################################################################
+############################################ KMS CONFIG ###########################################
+###################################################################################################
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "rds_key" {
+  deletion_window_in_days  = var.deletion_window_in_days
+  enable_key_rotation      = var.enable_key_rotation
+  description              = var.description
+
+  tags                     = {
+    Name: var.name
+  }
+}
+
+resource "aws_kms_alias" "rds_key_alias" {
+  name          = var.alias
+  target_key_id = join("", aws_kms_key.rds_key.*.id)
+}
--- main.tf
+++ main.tf
@@ -26,8 +26,16 @@ provider "aws" {
 #
 #    Use this module to deploy aws networking infrastructure.
 #
-module "deploy_vpc" {
-  source = "./vpc"
-  region   = "eu-west-1"
-  #  vpc_cidr = "192.168.0.0/16"
+#module "deploy_vpc" {
+#  source = "./vpc"
+#  region   = "eu-west-1"
+#  vpc_cidr = "192.168.0.0/16"
+#}
+
+#
+#    Use this module to deploy KMS manager.
+#
+module "deploy_kms" {
+  source = "./kms"
+  deletion_window_in_days = 7
 }
